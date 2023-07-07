213eab061660d9a329867dcb7339d2ab44b1457a
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-07-07 19:25:02 +0800

    upload/ register|login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,60 @@
 import React from 'react';
-// import DefaultLayout from '../components/DefaultLayout';
+import {Row, Col, From, Input, Form} from 'antd';
+import {Link} from 'react-router-dom';
+import {useDispatch} from 'react-redux';
+import { userLogin } from '../redux/actions/userActions';
+
 
 function Login () {
-  return (
-    <div>
 
-      <h1>Login Page</h1>
+  const dispatch = useDispatch()
+
+  function onFinish(values) {
+    dispatch(userLogin(values))
+
+    console.log("line:3", values);
+
+  }
+  return (
+    <div className="login">
+
+      <Row gutter={16} className="d-flex aligin-items-center">
+
+        <Col lg={16} style={{position: 'relative'}}>
+
+          <img src="https://images.unsplash.com/photo-1485291571150-772bcfc10da5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80" />
+          <h1 className="login-logo">SHEYCARS</h1>
+        </Col>
+        <Col lg={8} className="text-left p-5">
+          <Form layout="vertical" className="login-form p-5" onFinish={onFinish}>
+            <h1>Login</h1>
+            <hr />
+            <Form.Item
+              name="username"
+              label="Username"
+              rules={[{required: true}]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              label="Password"
+              rules={[{required: true}]}
+            >
+              <Input />
+            </Form.Item>
+
+            <button className="btn1 mt-2 mb-3">Login</button>
+
+            <br />
+
+            <Link to="/register">Click here to Register</Link>
+
+          </Form>
+        </Col>
+
+      </Row>
+
     </div>
   );
 }
