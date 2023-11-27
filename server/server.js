617b7b1a2f8655c096c10cdbaff04c0f2ba62b48
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-11-27 22:16:02 +0100

    upload

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const dbConnection = require('./db')
+const path = require('path');
 app.use(express.json());
 
 app.use('/api/cars/' , require('./routes/carsRoute'))
@@ -10,6 +11,9 @@ app.use('/api/users/' , require('./routes/usersRoute'))
 
 app.use('/api/bookings/' , require('./routes/bookingsRoute'))
 
-app.get('/', (req, res) => res.send('Hellow World'))
+// app.get('/', (req, res) => res.send('Hellow World'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 app.listen(port, () => console.log(`Node JS Server Started in Port ${port}`))
 
